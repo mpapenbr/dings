d8adf7996f8c9776b36745e73a460ffb019ace8f
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-02-17 23:06:03 +0100

    More features

--- cmd/root.go
+++ cmd/root.go
@@ -31,12 +31,7 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "dings",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Version: version.FullVersion,
 
 	// Uncomment the following line if your bare application
