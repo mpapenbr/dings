7aa478c6328174a795ff400634152d83bb0c7a5a
mpapenbr <Markus.Papenbrock+github@gmail.com>
2024-04-23 22:23:54 +0200

    Dummy stuff Fixes #32

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 Markus Papenbrock
+Copyright © 2024 Markus Papenbrock
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
